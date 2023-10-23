ca5cc1dcbd17a691f24476d104ee649f56f7ab22
Ivan Trajanovski <ivantrajanovski@gmail.com>
2023-10-23 11:39:34 +0200

    create add item form

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,30 +1,82 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list_flutter/data/dummy_items.dart';
+import 'package:shopping_list_flutter/models/grocery_item.dart';
+import 'package:shopping_list_flutter/widgets/new_item.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Your Groceries"),
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  final List<GroceryItem> _groceryItems = [];
+
+  void _addItem() async {
+    final newItem = await Navigator.of(context).push<GroceryItem>(
+      MaterialPageRoute(
+        builder: (context) => const NewItem(),
       ),
-      body: ListView.builder(
-        itemCount: groceryItems.length,
+    );
+
+    if (newItem == null) {
+      return;
+    }
+
+    setState(() {
+      _groceryItems.add(newItem);
+    });
+  }
+
+  void _removeItem(GroceryItem item) {
+    setState(() {
+      _groceryItems.remove(item);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = const Center(
+      child: Text("No items added yet."),
+    );
+
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: _groceryItems.length,
         itemBuilder: (context, index) {
-          final item = groceryItems[index];
-          return ListTile(
-            title: Text(item.name),
-            trailing: Text(item.quantity.toString()),
-            leading: Container(
-              width: 24,
-              height: 24,
-              color: item.category.color,
+          final item = _groceryItems[index];
+          return Dismissible(
+            onDismissed: (direction) {
+              _removeItem(item);
+            },
+            key: ValueKey(item.id),
+            child: ListTile(
+              title: Text(item.name),
+              trailing: Text(item.quantity.toString()),
+              leading: Container(
+                width: 24,
+                height: 24,
+                color: item.category.color,
+              ),
             ),
           );
         },
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Your Groceries"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              _addItem();
+            },
+          ),
+        ],
       ),
+      body: content,
     );
   }
 }
